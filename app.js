77a971af19484ae5b33058ef52cbaa4e7684f7ed
Tiven <vairtor@gmail.com>
2023-04-03 01:01:59 -0300

    :bulb: Add comments

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 require("dotenv").config();
 
 const CONTACT_ID = process.env.CONTACT_ID;
+
+/* CLIENT_ID and MESSAGE variables are completely optional, you can remove them and simply replace them with strings. */
 const CLIENT_ID = process.env.CLIENT_ID;
 const MESSAGE = process.env.MESSAGE;
 
 const client = new Client({
 	authStrategy: new LocalAuth({
-		clientId: CLIENT_ID,
+		clientId: CLIENT_ID /* Here you can replace it with "client name of your choice". */,
 		dataPath: "./session",
 	}),
 });
@@ -26,7 +28,10 @@ client.on("ready", () => {
 client.on("message", async (message) => {
 	if (message.body.toLowerCase() === "bot") {
 		contact = await client.getContactById(CONTACT_ID);
-		client.sendMessage(message.from, MESSAGE);
+		client.sendMessage(
+			message.from,
+			MESSAGE
+		); /* Here you can replace it with "messsage of your choice".*/
 		client.sendMessage(message.from, contact);
 	}
 });
